c5aba508b46fb636bab3f0c67c749f93a0d46933
Emir Maljanovic <emir.maljanovic@gmail.com>
2019-08-27 09:05:22 +0200

    Removed unused code

--- src/helpers/fetch.js
+++ src/helpers/fetch.js
@@ -1,17 +1,9 @@
-export const REDIRECT_URI = 'http://localhost';
-export const CLIENT_ID = '';
+const CLIENT_ID = '';
 
 const FETCH_HEADER = {
   'Client-ID': CLIENT_ID,
   'Accept': 'application/vnd.twitchtv.v5+json',
   'Content-Type': 'application/json; charset=utf-8'
-};
-
-export const getTokenFromUrl = () => {
-  const hasToken = window.location.href.indexOf('access_token') > -1;
-  const token = hasToken ? window.location.href.substring(window.location.href.indexOf('access_token') + 13, window.location.href.indexOf('scope') - 1) : null;
-
-  return token;
 };
 
 export const get = ({ url }) =>
